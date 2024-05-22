8e5c8942eae43147f9df52900fffbfb56dfacb18
SaranshWeb <100319441+SaranshWeb@users.noreply.github.com>
2024-05-22 15:29:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Component/Footer';
+
+import Header from './Component/Header';
+import Main from './Component/Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main></Main>
+      <Footer></Footer>
+    </>
+
+  );
+}
+
+export default App;
